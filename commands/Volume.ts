efc777c9a1689c0138da179d03d2e52ca1a21bcb
Gandalf <asoc04@hotmail.com>
2020-08-04 20:53:44 +0300

    volume send

--- commands/Volume.ts
+++ commands/Volume.ts
@@ -7,18 +7,24 @@ class Volume extends Command {
     public aliases: string[] = ["ses", "vol"];
     async execute(msg: import("discord.js").Message, args: string[]): Promise<void> {
         let level = parseFloat(args[0]);
-        if (!level) {
-            msg.channel.send("Enter a number eg.(0.5)").then((msg1) => {
-                msg1.delete({ reason: "cleanup", timeout: (30 * 1000) });
-            });
-            return;
-        }
-
+        
+        
         let guildData = vault.guildData.get(msg.guild!.id)!;
         if (!guildData.queue.connection) {
             msg.channel.send("Bot needs to be present to change the volume");
             return;
         }
+
+        if (!level) {
+            // msg.channel.send("Enter a number eg.(0.5)").then((msg1) => {
+            //     msg1.delete({ reason: "cleanup", timeout: (30 * 1000) });
+            // });
+            msg.channel.send(`Current Volume:${guildData.queue.volume}`).then((msg1) => {
+                msg1.delete({ reason: "cleanup", timeout: (20 * 1000) });
+            });
+            return;
+        }
+
         if (!guildData.queue.connection!.dispatcher) {
             msg.channel.send("Nothing currently playing cany change the volume");
             return;
